e150f639484f7f0a27bed0d11c857e497c268017
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-10 14:21:38 -0700

    infinite add

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
-from  decimal import Decimal
+from decimal import Decimal
 if __name__ == "__main__":
     args = sys.argv[1:]
     result = sum(Decimal(arg) for arg in args)
