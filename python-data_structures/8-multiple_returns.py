bffe0aa7042e34d8bf9466439f6a326f3d8b5693
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-14 11:54:56 -0700

    implement function that prints length and first Char

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    """
+    This function takes a string as an argument.
+    If string is empty, return a tuple where first element is 0
+    and second element is None.
+    If string not empty, return a tuple where first element is length of string
+    and the second element is first character of string.
+    """
+    if not sentence:
+        return 0, None
+    else:
+        return len(sentence), sentence[0]
