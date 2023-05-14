cec8a3ffcbf76b959752b4193dc343e42695aa17
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-14 11:39:47 -0700

    implement add_tuple function

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = tuple_a + (0, 0)[:2 - len(tuple_a)]
+    b = tuple_b + (0, 0)[:2 - len(tuple_b)]
+    sum_tuple = (a[0] + b[0], a[1] + b[1])
+    return (sum_tuple)
