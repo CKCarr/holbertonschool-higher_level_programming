fda7a6e06d31f0c013a0ea7a8de5253ba62929af
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-13 20:43:31 -0700

    implement element at function

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+""" check if idx is negative or greater than or equal to the length of my_list"""
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return (None)
+    return (my_list[idx])
