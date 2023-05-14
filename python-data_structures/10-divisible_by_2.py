6c6d3a8fc58c23426f5efae334cbc3aeb9eba76b
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-14 13:21:43 -0700

    implement function divisible_by_2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    """Using list comprehension to iterate over my_list
+    and check if each element is divisible by 2"""
+    return [num % 2 == 0 for num in my_list]
