f897e45c43e8b3119646ed381e3abc390b5e8f6d
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-13 20:47:54 -0700

    style fix

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-""" check if idx is negative or greater than or equal to the length of my_list"""
 def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return (None)
