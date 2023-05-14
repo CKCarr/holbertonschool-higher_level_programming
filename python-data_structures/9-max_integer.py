8d19609df5c9f3697bcb2cf94c85dc069f1635fe
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-14 12:15:15 -0700

    implement function max_integer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    # if list empty, return none
+    if len(my_list) == 0:
+        return (None)
+    # initialize variable to store max num of list
+    max_num = my_list[0]
+    # iterate through list
+    for num in my_list[1:]:
+        # if current num is greater than current max
+        if num > max_num:
+            # update variable to store new max num
+            max_num = num
+            # return the final max num
+            return (max_num)
