f9e492a21da1f20176c7c1cc64d8171b10807d28
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-13 22:37:59 -0700

    implement replace_in_list: print_reversed_list: no_c: print_matrix_integer: functions and tested with main modules as well

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for idx, element in enumerate(row):
+            if idx != 0:
+                print(" ", end="")
+            print("{:d}".format(element), end="")
+        print()
