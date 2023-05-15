4bd35736bbec1d737734ecec05285f4407bd8004
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-14 22:16:17 -0700

    implement number_keys function that returns the number of keys in a dictionary.

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """function that returns the number
+    of keys in a dictionary.
+    Args:
+        a_dictionary (dict): The dictionary.
+    Returns:
+        int: the number of keys in the dictionary
+    """
+    return (len(a_dictionary))
