b6758f1a09f074e29a21b653b27167eacc172928
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-14 23:14:24 -0700

    retry

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -5,4 +5,7 @@ def update_dictionary(a_dictionary, key, value):
     Args:
         a_dictionary (dict): The dictionary
     """
-    a_dictionary.update({key: value})
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary.update({key: value})
