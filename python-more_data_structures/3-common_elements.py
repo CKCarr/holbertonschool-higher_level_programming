05719d0a54594f97891535035fa2c61f1237753b
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-14 21:51:05 -0700

    ls

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """  function that returns a set of
+         common elements in two sets
+    Args:
+        set_1 (set): The first set
+        set_2 (set): The second set
+    """
+    return (set_1.intersection(set_2))
