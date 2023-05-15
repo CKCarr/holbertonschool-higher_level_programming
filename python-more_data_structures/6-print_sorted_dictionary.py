61a12b3012751d9dbc7c8748d4a0d5f71a81a62c
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-14 22:31:10 -0700

    implement  print_sorted_dictionary function that prints a dictionary by ordered keys.

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    """ function that prints a dictionary by ordered keys.
+    Args:
+        a_dictionary (dict): the dictionary
+    """
+    # sorted returns a list of keys sorted in ascending order.
+    sorted_keys = sorted(a_dictionary)
+    # iterate through sorted dict
+    for key in sorted_keys:
+        # print key : value pair
+        print(key, ":", a_dictionary[key])
