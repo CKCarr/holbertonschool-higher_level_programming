1f5885dd208ad52f272d3db4aecab4ca3362364d
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-14 22:56:10 -0700

    implement update_dictionary function that replaces or adds key/value in a dictionary

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,6 +4,9 @@ def print_sorted_dictionary(a_dictionary):
     Args:
         a_dictionary (dict): the dictionary
     """
+    if a_dictionary is None:
+        print("Invalid dictionary!")
+        return
     # sorted returns a list of keys sorted in ascending order.
     sorted_keys = sorted(a_dictionary)
     # iterate through sorted dict
---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """function that replaces or adds
+    key/value in a dictionary.
+
+    Args:
+        a_dictionary (dict): The dictionary
+    """
+    a_dictionary[key] = value
