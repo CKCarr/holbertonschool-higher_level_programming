ab1f22bacae7fe1807375ff1f320b4015ebd9e55
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-16 16:35:46 -0700

    implement safe list print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
-        for i in range(x):
-            print(my_list[i], end=" ")
+        while True:
+            print(my_list[count], end=" ")
+            count += 1
+            if count == x:
+                break
     except IndexError:
         pass
     finally:
         print()
-        return min(x, len(my_list))
+        return count
