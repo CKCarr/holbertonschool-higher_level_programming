befe48a26c904af4e587b40f7a12e16903c017d2
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-16 19:54:52 -0700

    fix spacing opep8 list division

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = []
-    for idx in range (list_length):
+    for idx in range(list_length):
         res = 0
         try:
             res = my_list_1[idx] / my_list_2[idx]
