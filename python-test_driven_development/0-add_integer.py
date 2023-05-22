905718ec03180e84a7ab9e44a85663cc698fbffd
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-21 21:50:23 -0500

    attempt to test add_integer function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -27,9 +27,9 @@ def add_integer(a, b=98):
         ...
     a must be an integer
     """
-    if a is None or (type(a) != int and type(a) != float):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if b is None or (type(b) != int and type(b) != float):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
