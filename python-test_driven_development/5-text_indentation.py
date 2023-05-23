d021e7b1ff67fc1bb4d8425010efd38986c9e68f
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-22 23:08:25 -0700

    test 5 text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+module writes a function that prints a text with
+2 new lines after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    This function prints a text with 2 new lines
+    after each of these characters: ., ? and :
+
+    Parameters:
+    text (str): The text to be indented
+
+    Returns:
+    None
+
+    Raises:
+    TypeError: If text is not a string
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    text = text.replace(".", ".\n\n")
+    text = text.replace("?", "?\n\n")
+    text = text.replace(":", ":\n\n")
+
+    lines = text.split('\n')
+    for i in range(len(lines)):
+        print(lines[i].strip())
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
