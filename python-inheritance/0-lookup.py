9196a5cb42dbde9e22d25ddc04977bb7e2c895c2
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-27 12:57:21 -0500

    test pycharm
    pycharm test

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """ function that returns the list of
+    available attributes and methods of an object.
+
+    :param obj: (object): The object to inspect.
+    :return: list: A list containing the names of attributes and methods.
+    """
+    attr_and_meth = dir(obj)
+    return attr_and_meth
