670c0d5b32cf02c2851cd8722e5d59f9b8687ee1
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-27 21:57:14 -0500

    Geometry mnodule with function area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module containing the definition for class 'BaseGeometry'.
+Public instance method: def area(self):
+Raises:  Exception: with the message "area() is not implemented"
+"""
+
+
+class BaseGeometry:
+    """A class called 'BaseGeometry' that contains an unimplemented method."""
+
+
+def area(self):
+    """ Function Raises an exception when called.
+    indicating that this method has not yet been implemented.
+    """
+    raise Exception("area() is not implemented")
