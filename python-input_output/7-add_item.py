33f53b4241f77eb59c96f4e04a46dc46faff6afd
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-29 18:19:44 -0500

    .

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
 """ This script adds all arguments to a Python list and saves them to a file.
 It uses the save_to_json_file function from 5-save_to_json_file.py and the
 load_from_json_file function from 6-load_from_json_file.py.
 The list is saved as a JSON representation in a file named add_item.json.
 If the file doesn’t exist, it is created. The script does not handle file
-permissions or exceptions.
-"""
+permissions or exceptions. """
 
 
 import sys
