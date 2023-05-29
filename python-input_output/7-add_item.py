8a2ee7ee38e5e34e0bb7ba2e406875d63a30696f
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-29 18:42:14 -0500

    for real this time

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,17 +6,28 @@ The list is saved as a JSON representation in a file named add_item.json.
 If the file doesn’t exist, it is created. The script does not handle file
 permissions or exceptions. """
 import sys
+import os.path
+
+"""Import functions from other python files
+__import__() used because standard import statement doesn't work
+with filenames starting with a number. """
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
+# Initialize an empty list
+my_list = []
 
-try:
-    data_to_file = load_from_json_file(filename)
-except ValueError:
-    data_to_file = []
+# Check if the file "add_item.json" exists
+if os.path.isfile("add_item.json"):
+    # If the file exists, load the data from the file
+    my_list = load_from_json_file("add_item.json")
 
-data_to_file.extend(sys.argv[1:])
+# Loop through the command line args starting from index 1, exclude script name
+for i in range(1, len(sys.argv)):
+    # Append each argument to the list
+    my_list.append(sys.argv[i])
 
-save_to_json_file(data_to_file, filename)
+# Save the updated list back to the file
+# If the file doesn't exist, it will be created at this point.
+save_to_json_file(my_list, "add_item.json")
