cad36a2bd3b8e86bdee2494f59d23a7ffdbd4e6a
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-29 18:12:19 -0500

    so Instead of defining a variable and all the extrea i wrote literally a script also I shall try this as import instead of all the other ways I have used that do not seem to be appreciated by Check

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,66 +1,30 @@
 #!/usr/bin/python3
 
-""" This module writes a script that adds all arguments to a Python list,
-and then save them to a file.
-You must use your function save_to_json_file from 5-save_to_json_file.py.
-You must use your function load_from_json_file from 6-load_from_json_file.py.
-The list must be saved as a JSON representation in a file named add_item.json.
-If the file doesn’t exist, it should be created
-Does not manage file permissions / exceptions.
+""" This script adds all arguments to a Python list and saves them to a file.
+It uses the save_to_json_file function from 5-save_to_json_file.py and the
+load_from_json_file function from 6-load_from_json_file.py.
+The list is saved as a JSON representation in a file named add_item.json.
+If the file doesn’t exist, it is created. The script does not handle file
+permissions or exceptions.
 """
 
 import sys
-import importlib
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file = importlib.import_module("5-save_to_json_file")
-load_from_json_file = importlib.import_module("6-load_from_json_file")
-# Load the code from file as a string
-with open("5-save_to_json_file.py", "r") as file:
-    save_to_json_code = file.read()
+filename = "add_item.json"
+# Read existing data from add_item.json or create an empty list
+try:
+    data = load_from_json_file(filename)
+except FileNotFoundError:
+    data = []
 
-with open("6-load_from_json_file.py", "r") as file:
-    load_from_json_code = file.read()
+# Append the command-line arguments to the list
+data.extend(sys.argv[1:])
 
-# Execute the module code and capture the resulting globals dictionary
-save_to_json_globals = {}
-exec(save_to_json_code, save_to_json_globals)
+# Save the updated list to add_item.json
+save_to_json_file(data, filename)
 
-load_from_json_globals = {}
-exec(load_from_json_code, load_from_json_globals)
-
-# Access the functions from the global dictionary
-save_to_json_file = save_to_json_globals["save_to_json_file"]
-load_from_json_file = load_from_json_globals["load_from_json_file"]
-
-
-# function takes a list of command line arguments
-def add_args_to_list(args):
-    """ Function that adds all command line arguments to a Python List.
-
-    Args:
-        args (list): The command line arguments to be added to the list.
-    Return: (list): The updated list.
-    Raises: None
-    """
-    # 'data' read from JSON file and stored in variable
-    try:
-        data = load_from_json_file('add_item.json')
-    # If the file doesn’t exist, it should be created
-    except FileNotFoundError:
-        data = []
-
-    # extend method is used to append multiple elements to a list.
-    data.extend(args)
-    # save the updated list to JSON representation in a file
-    save_to_json_file(data, "add_item.json")
-    return data
-
-
-if __name__ == "__main__":
-    # Extract command line arguments excluding the script name argv[1:...]
-    args = sys.argv[1:]
-
-    # Add args to list, and save updated list to add_item.json
-    updated_arg_list = add_args_to_list(args)
-    print(updated_arg_list)
+# Print the updated list
+print(data)
