03bb8ccf7171cb950cd6674fcb9ae40355ddd825
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-05-29 21:29:45 -0500

    9. Student to JSON : class Student that defines a student

---
+++ python-input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This module Writes a class Student that defines a student.
+
+    Public instance attributes:
+        and instantiation with: 
+            first_name: (str) "first name of student"
+            last_name: (str) "last name of student"
+            age: (int)  age of a student
+    Public method: 'to_json' retrieves a dictionary representation 
+    of a Student instance
+    Returns: dictionary of a student instance """
+
+
+class Student:
+    """ A class to represent a Student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Retrieves a dictionary representation of a Student instance 
+
+        Returns:
+            dict: 
+        """
+        return self.__dict__
