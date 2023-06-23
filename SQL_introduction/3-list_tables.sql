60970d90993926e33be551a2fc75b0fcb18fe940
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-06-23 09:09:02 -0700

    3. List tables

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,6 @@
+-- SQL script that lists all the tables of a database in your MySQL server.
+-- database name will be passed as argument of mysql command.
+
+-- list all mysql database tables
+USE mysql;
+SHOW tables;
