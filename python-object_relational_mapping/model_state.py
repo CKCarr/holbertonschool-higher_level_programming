3733095633e87de3416d1d20ad7ceb6a3d35ae5a
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-06-26 15:39:04 -0700

    6. First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,8 +5,8 @@
     to work with MySQLAlchemy ORM.
 """
 
-from SQLAlchemy import Column, Integer, String
-from SQLAlchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
