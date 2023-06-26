019c68048b2e6a467866b2d82d71c832087060e9
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-06-26 07:34:02 -0700

    0. Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-# SQL script that lists all states from the database hbtn_0e_0_usa:
+"""
+SQL script that lists all states from the database hbtn_0e_0_usa:
+
+"""
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    """
+    
+    """
     # Get command line arguments
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
