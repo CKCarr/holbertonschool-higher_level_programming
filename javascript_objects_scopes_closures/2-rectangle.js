eb8b74eb7a87179b0c856993a41fb93921273cb7
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-07-29 15:36:17 -0700

    test 2 to check output

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -9,17 +9,17 @@ Write a class Rectangle that defines a rectangle:
  */
 
 class Rectangle {
+  /* Rectangle class with 2 args w & h */
   constructor (w, h) {
     const isNonPositiveInt = (w <= 0 || h <= 0);
     const isNotInt = (!Number.isInteger(w) || !Number.isInteger(h));
 
     if (isNonPositiveInt || isNotInt) {
       // If w or h = 0 or not positive ints, create an empty object
-      return {};
+    } else {
+      this.width = w;
+      this.height = h;
     }
-
-    this.width = w;
-    this.height = h;
   }
 }
 
