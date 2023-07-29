5e1cb606ebecbf72b26e90ef1065f73da57408a8
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-07-29 10:22:05 -0700

    initialize repo with environment set up for JavaScript - Objects, Scopes and Closures

---
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* 0. Rectangle #0
+Write an empty class Rectangle that defines a rectangle:
+    You must use the class notation for defining your class
+ */
+
+class Rectangle {
+    // This is an empty Rectangle class
+  }
+
+  module.exports = Rectangle;
