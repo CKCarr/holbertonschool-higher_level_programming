8b648ec340cec3dc863239825595fea3989956bb
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-07-31 07:08:15 -0700

    javascript_objects_scopes_closures : closed

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -4,6 +4,9 @@ Write a function that prints the number of arguments already printed and the new
     Prototype: exports.logMe = function (item)
     Output format: <number arguments already printed>: <current argument value>
  */
+// count to keep track of arguments already printed
+let count = 0;
 exports.logMe = function (item) {
-
+  console.log(count + ':' + item);
+  count++;
 };
