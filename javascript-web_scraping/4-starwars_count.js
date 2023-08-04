9009ef4eda1f3179ed335ad0a40bc3f5e682bbd2
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-08-04 11:18:35 -0700

    5. Loripsum

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,36 @@
 #!/usr/bin/node
 /* Write a script that prints the number of movies where the character “Wedge Antilles” is present.
-
-    The first argument is the API URL of the Star wars API: https://swapi-api.hbtn.io/api/films/
-    Wedge Antilles is character ID 18 - your script must use this ID for filtering the result of the API
+    The first argument is the API URL of the Star wars API:
+    https://swapi-api.hbtn.io/api/films/
+    Wedge Antilles is character ID 18 -
+    your script must use this ID for filtering the result of the API
     You must use the module request
 */
+// required modules
+const request = require('request');
+// first argument is the API URL
+const apiUrl = process.argv[2];
+
+// function to count movies with "Wedge Antilles" char ID 18 present
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    return console.log(error);
+  }
+  // parse body as JSON and get results array
+  const parsedBody = JSON.parse(body).results;
+  // initialize count to keep track of occurrences
+  let count = 0;
+  // loop through the results array
+  for (let i = 0; i < parsedBody.length; i++) {
+    // find characters array in each 'results' element
+    const characters = parsedBody[i].characters;
+    // check if character array has ID 18
+    const personID18 = characters.find((person) => person.includes('18'));
+    // if a character with ID 18 is found increment the count
+    if (personID18) {
+      count++;
+    }
+  }
+  // Print the final count of occurrences of characters with ID 18
+  console.log(count);
+});
--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -6,3 +6,26 @@
     The file must be UTF-8 encoded
     You must use the module request
 */
+// import modules
+const fs = require('fs');
+const request = require('request');
+
+// command line arguments
+const apiUrl = process.argv[2];
+const filePath = process.argv[3];
+
+// request given url with a callback function for error or success of url
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error.message);
+  } else {
+    // use fs to write to file w/ callback function upon error or success
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log('File written successfully.');
+      }
+    });
+  }
+});
