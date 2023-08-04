6b663a9a131e2ff8382a03516d88668a9f77a97c
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-08-03 20:49:36 -0700

    2. Status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -5,3 +5,17 @@
     The status code must be printed like this: code: <status code>
     You must use the module request
  */
+// import module request module to make HTTP requests
+const request = require('request');
+// array of cmd line args: arg[2] is url, to request [GET]
+const url = process.argv[2];
+
+// two args, url request && callback function for error and status code
+request(url, (error, response) => {
+  if (error) {
+    console.error(error.message);
+  } else {
+    // 200: for success || 404: for not found
+    console.log(`code: ${response.statusCode}`);
+  }
+});
