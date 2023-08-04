0be49d5c04e84e91befb5e2d9b2d61b780e07f3e
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-08-04 11:19:57 -0700

    Correction of "5. Loripsum"

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -16,16 +16,14 @@ const filePath = process.argv[3];
 
 // request given url with a callback function for error or success of url
 request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error.message);
-  } else {
-    // use fs to write to file w/ callback function upon error or success
-    fs.writeFile(filePath, body, 'utf-8', (err) => {
-      if (err) {
-        console.error(err.message);
-      } else {
-        console.log('File written successfully.');
-      }
-    });
-  }
+    if (error) {
+        console.error(error.message);
+    } else {
+        // use fs to write to file w/ callback function upon error or success
+        fs.writeFile(filePath, body, 'utf-8', (err) => {
+            if (err) {
+                console.error(err.message);
+            }
+        });
+    }
 });
