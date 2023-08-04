e3b4f7ad1794e81d8c1ec6efc4db417d004c8796
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-08-04 11:21:18 -0700

    semistandard validation

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -16,14 +16,14 @@ const filePath = process.argv[3];
 
 // request given url with a callback function for error or success of url
 request(apiUrl, (error, response, body) => {
-    if (error) {
-        console.error(error.message);
-    } else {
-        // use fs to write to file w/ callback function upon error or success
-        fs.writeFile(filePath, body, 'utf-8', (err) => {
-            if (err) {
-                console.error(err.message);
-            }
-        });
-    }
+  if (error) {
+    console.error(error.message);
+  } else {
+    // use fs to write to file w/ callback function upon error or success
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(err.message);
+      }
+    });
+  }
 });
