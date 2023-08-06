196c6d6bd796c7eb6c66f60ad6f1b6fac83d1fec
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-08-06 12:24:59 -0700

    cors wont allow to fetch

--- javascript-web_jquery/9-script.js
+++ javascript-web_jquery/9-script.js
@@ -7,22 +7,10 @@ Write a JavaScript script that fetches from https://stefanbohacek.com/hellosalut
     You must use the JQuery API
     Your script must work when it is imported from the <head> tag
  */
-function displayHello(data) {
-    $('#hello').text(data.hello);
-}
-
 $(document).ready(function () {
-    $.ajax({
-        url: 'https://cors-anywhere.herokuapp.com/https://stefanbohacek.com/hellosalut/?lang=fr',
-        dataType: 'jsonp',
-        jsonpCallback: 'displayHello',
-        success: function (data) {
-            // This function will be executed when the JSONP response is received.
-            // The JSONP response should contain the "hello" data.
-        },
-        error: function () {
-            $('#hello').text('Error fetching data');
-        }
+    $.get('https://stefanbohacek.com/hellosalut/?lang=fr', function (data) {
+        $('#hello').text(data.hello);
+    }).fail(function () {
+        $('#hello').text('Error fetching data');
     });
 });
-
--- javascript-web_jquery/server.js
+++ javascript-web_jquery/server.js
@@ -1,27 +1,41 @@
-#!/usr/bin/node
 const express = require('express');
-const axios = require('axios');
+const https = require('https');
 
 const app = express();
 const port = 3000;
 
-// Add the necessary CORS header to allow all origins (for testing purposes)
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
 });
 
-app.get('/hello', async (req, res) => {
-    try {
-        const response = await axios.get('https://stefanbohacek.com/hellosalut/?lang=fr');
-        res.json({ hello: response.data.hello });
-    } catch (error) {
+app.get('/hello', (req, res) => {
+    const options = {
+        hostname: 'stefanbohacek.com',
+        path: '/hellosalut/?lang=fr',
+        method: 'GET'
+    };
+
+    const request = https.request(options, (response) => {
+        let data = '';
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        response.on('end', () => {
+            const hello = JSON.parse(data).hello;
+            res.json({ hello });
+        });
+    });
+
+    request.on('error', (error) => {
         console.error(error.message);
         res.status(500).json({ error: 'Error fetching data' });
-    }
+    });
+
+    request.end();
 });
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-
